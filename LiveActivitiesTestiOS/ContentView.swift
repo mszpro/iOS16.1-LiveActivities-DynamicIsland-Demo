653f4bafe688698c79eef4aeb5835fa22af6d6e8
Shunzhe <me@sora-gate.com>
2022-09-02 14:22:46 +0900

    Initial Commit

---
+++ LiveActivitiesTestiOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LiveActivitiesTestiOS
+//
+//  Created by Sora on 2022/09/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LiveActivitiesTestiOS/LiveActivitiesTestiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivitiesTestiOSApp.swift
+//  LiveActivitiesTestiOS
+//
+//  Created by Sora on 2022/09/02.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivitiesTestiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
